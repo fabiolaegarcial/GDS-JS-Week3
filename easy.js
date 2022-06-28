687fd07223a63440fce711df2c3173367dd624af
fabiolaegarcial <67425045+fabiolaegarcial@users.noreply.github.com>
2022-06-27 21:19:51 -0500

    fixed file for veryEasy

--- easy.js
+++
@@ -1,4 +0,0 @@
-var num1 = 6;
-var num2 = 4;
-
-console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`);
--- veryEasy.js
+++ veryEasy.js
@@ -1,1 +1,4 @@
+var num1 = 6;
+var num2 = 4;
 
+console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`);
