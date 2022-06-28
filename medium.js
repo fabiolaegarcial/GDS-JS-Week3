f825a8c5d163016ffbc44453007e782500af2119
fabiolaegarcial <67425045+fabiolaegarcial@users.noreply.github.com>
2022-06-27 21:43:05 -0500

    finished medium challenge

--- medium.js
+++ medium.js
@@ -1,1 +1,12 @@
+var userInput = prompt("SHOUT, whisper, Or Talk Normally:");
 
+var isUpper = userInput.toUpperCase() === userInput;
+var isLower = userInput.toLowerCase() === userInput;
+
+if (isLower === true){
+    console.log(`you were whispering`);
+} else  if (isUpper === true){
+    console.log(`YOU WERE SHOUTING!`);
+} else {
+    console.log(`You were speaking normally.`);
+}
