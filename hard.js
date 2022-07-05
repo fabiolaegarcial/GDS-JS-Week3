9ff74ddd8dd20754b333bd41a1b6dd0b5cab648e
fabiolaegarcial <67425045+fabiolaegarcial@users.noreply.github.com>
2022-07-04 21:56:14 -0500

    completed 'hard task'

--- hard.js
+++ hard.js
@@ -1,1 +1,17 @@
+//  Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
 
+function add(operand1, operand2){
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2){
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2){
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2){
+    return operand1 / operand2;
+}
--- veryHard.js
+++ veryHard.js
@@ -1,1 +1,9 @@
+let months = ["January", "February", "March"," April", "May", "June","July","August","September","October","November","December"];
 
+let numMonth = 3; numMonth--;
+
+if (numMonth < 12 && numMonth >= 0){
+    console.log(`Month number ${numMonth + 1} is ${months[numMonth]}`);
+} else {
+    console.log("Try again.")
+}
